702fa14bc0d5678236232e4f3598a4ce2970de07
Larios <alexlarios170498@gmail.com>
2025-03-17 16:41:07 -0600

    deplocloudabort

--- config/database.ts
+++ config/database.ts
@@ -1,19 +1,21 @@
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '35.234.149.190'),
-      // socketPath: `/cloudsql/${env('DATABASE_HOST')}`,
+      //  host: env('DATABASE_HOST', '35.234.149.190'),//para levantar local
+      host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,//usar para produccion
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'dcphonedb'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'HolaMundo17X..'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: env.bool('DATABASE_SSL', false) && {
         key: env('DATABASE_SSL_KEY', undefined),
-        cert: env('DATABASE_SSL_CERT', undefined),
-        ca: env('DATABASE_SSL_CA', undefined),
-        capath: env('DATABASE_SSL_CAPATH', undefined),
-        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        // cert: env('DATABASE_SSL_CERT', undefined),
+        // ca: env('DATABASE_SSL_CA', undefined),
+        // capath: env('DATABASE_SSL_CAPATH', undefined),
+        // cipher: env('DATABASE_SSL_CIPHER', undefined),
         rejectUnauthorized: env.bool(
           'DATABASE_SSL_REJECT_UNAUTHORIZED',
           true
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default ({ env }) => ({
+
+    'users-permissions': {
+        config: {
+            jwtSecret: env('jwtSecret','fy0wjAbfVSSNIVNcI3PjFQ=='),
+        },
+
+    },
+      'documentation': {
+    config: {
+      path: '/tmp/documentation', // Cambia la ruta a una con permisos de escritura
+    },
+  },
+});
+
+// export default {
+//     'users-permissions': {
+//       config: {
+//         jwtSecret: process.env.JWT_SECRET || 'tu-secreto-aqui',
+//       },
+//     },
+//   };
