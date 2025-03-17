18f4daf382fba225b5a80db463a35405e28f41af
Larios <alexlarios170498@gmail.com>
2025-03-16 18:27:26 -0600

    deploycloud

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT'), // Permite que Azure asigne el puerto dinámicamente
+  port: env.int('PORT'), 
   app: {
     keys: env.array('APP_KEYS'),
   }
