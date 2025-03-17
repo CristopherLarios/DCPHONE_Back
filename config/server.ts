a43bbd240adb31157c7157e4e485b7a28441c0d2
Larios <alexlarios170498@gmail.com>
2025-03-17 17:59:50 -0600

    deployvm env

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT',80),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('ADMIN_JWT_SECRET','1NJSycfSLFvvWPGUTebsdg=='),
   }
 });
